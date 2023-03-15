568aff5ef98c64316d8be3e8f746eaec1afd7311
LordCracken <vladislav070201@gmail.com>
2023-03-15 19:13:26 +0300

    add public folder with html and styles and serve it from express

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
+app.use(express.static('public'));
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
